70afff34b704720c7520e5eb51e4a74072d74096
pedro <pnarabelo@gmail.com>
2022-05-25 16:20:20 -0300

    chore: configuração paginação

---
+++ backend/src/products/products.controller.ts
@@ -0,0 +1,74 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { ApiPageResponse } from 'src/page/api-page-response';
+import { ConnectionArgsDto } from 'src/page/connection-args.dto';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { ProductEntity } from './entities/product.entity';
+import { ProductsService } from './products.service';
+
+@Controller('products')
+@ApiTags('products')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Post()
+  @ApiCreatedResponse({ type: ProductEntity })
+  async create(@Body() createProductDto: CreateProductDto) {
+    return new ProductEntity(
+      await this.productsService.create(createProductDto),
+    );
+  }
+
+  @Get()
+  @ApiOkResponse({ type: [ProductEntity] })
+  async findAll() {
+    const products = await this.productsService.findAll();
+    return products.map((product) => new ProductEntity(product));
+  }
+
+  @Get('page')
+  @ApiPageResponse(ProductEntity)
+  async findPage(@Query() connectionArgs: ConnectionArgsDto) {
+    return this.productsService.findPage(connectionArgs);
+  }
+
+  @Get('drafts')
+  @ApiOkResponse({ type: [ProductEntity] })
+  async findDrafts() {
+    const drafts = await this.productsService.findDrafts();
+    return drafts.map((product) => new ProductEntity(product));
+  }
+
+  @Get(':id')
+  @ApiOkResponse({ type: ProductEntity })
+  async findOne(@Param('id') id: string) {
+    return new ProductEntity(await this.productsService.findOne(id));
+  }
+
+  @Patch(':id')
+  @ApiCreatedResponse({ type: ProductEntity })
+  async update(
+    @Param('id') id: string,
+    @Body() updateProductDto: UpdateProductDto,
+  ) {
+    return new ProductEntity(
+      await this.productsService.update(id, updateProductDto),
+    );
+  }
+
+  @Delete(':id')
+  @ApiOkResponse({ type: ProductEntity })
+  async remove(@Param('id') id: string) {
+    return new ProductEntity(await this.productsService.remove(id));
+  }
+}
